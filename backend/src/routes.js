583867fc1da96eb86019b8d291810fbe672dea5d
gnf22 <gnferreira2000@gmail.com>
2020-03-29 18:05:35 -0300

    Testes da aplicação.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -15,7 +15,6 @@ routes.post('/ongs', OngController.create)
 
 routes.get('/profile', ProfileController.index);
 
-routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
 module.exports = routes;
